9e3ba05baf84b00977fd61b70be15c85b191e308
varshith <ReddemVarshith>
2026-01-07 11:34:34 +0530

    Donations Page1

--- backend/shaaka_backend/settings.py
+++ backend/shaaka_backend/settings.py
@@ -125,6 +125,10 @@ STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+# Media files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
