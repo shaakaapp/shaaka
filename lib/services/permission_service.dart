93e918dfa72727cce65c5ad655d2983310334619
ReddemVarshith <reddemvarshith@gmail.com>
2026-02-22 18:12:38 +0530

    Fix: bypass permission_handler on Web to prevent UnsupportedError crash on startup

--- lib/services/permission_service.dart
+++ lib/services/permission_service.dart
@@ -1,7 +1,11 @@
+import 'package:flutter/foundation.dart';
 import 'package:permission_handler/permission_handler.dart';
 
 class PermissionService {
   static Future<void> requestAppPermissions() async {
+    // permission_handler doesn't support web and will throw UnsupportedError
+    if (kIsWeb) return;
+
     // Request multiple permissions at once.
     // If they are already granted, this will just return quickly.
     await [
